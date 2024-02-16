a72d061b42f5ddbea4dd6f0bcb407484075c187d
Francisco Hernandez <paco049600@gmail.com>
2024-02-15 23:51:52 -0600

    Cambio de estructura

--- app.js
+++ app.js
@@ -1,75 +1,109 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const {
+    createBot,
+    createProvider,
+    createFlow,
+    addKeyword,
+    EVENTS } = require('@bot-whatsapp/bot')
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
+const MongoAdapter = require('@bot-whatsapp/database/mongo')
+//const { createBotDialog } = require('@bot-whatsapp/contexts/dialogflow');
+/**
+ * Declaramos las conexiones de Mongo
+ */
+const MONGO_DB_URI = 'mongodb+srv://anaymiachatbot:123456anaymia@projectanaymia.yiho2sz.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_DB_NAME = 'anaymiaDB'
 
+// Inicia con las interacciones del usuario
+const flowResponseOk = addKeyword(
+    [
+        'Si',
+        'Si, me gustaría',
+        'Claro',
+        'Con gusto',
+        'Simon'
+    ])
+    .addAnswer(
+        [
+            'Cuéntanos sobre tu día.',
+            'Somos todo oídos'
+        ]);
 
-const botondesentimiento= addKeyword('¿Cómo te sientes ?').addAnswer('Elige tu estado de animo', {
-    buttons: [{ body: 'Bien :)' }, { body: 'Mal :(' }, { body: 'Triste :((' }, { body: 'Muy Feliz :)' }, { body: 'No muy bien :Z' }],
-})
+const flowBat = addKeyword(
+    [
+        'Mal',
+        'Me siento pésimo',
+        'Me ha ido muy mal',
+        'No tan bien'
+    ])
+    .addAnswer('Todo estará bien, ¿te gustaría platicarlo?', {
+        delay: 1500
+    }, null, flowResponseOk);
 
-//CREACIÓN DE INTENT " ORIENTACION CON PROFESIONALES"
+const flowGood = addKeyword(
+    [
+        'Bien',
+        'Grandioso',
+        'Muy bien',
+        'Genial',
+        'Chido'
+    ])
+    .addAnswer('Nos da gusto saberlo.')
+    .addAnswer('¿Quieres platicarnos más sobre tu día?', {
+        delay: 1500
+    }, null, flowResponseOk);
 
+const flowAutoAtact = addKeyword(
+    [
+        'Nadie me querrás así',
+        'Estoy obeso',
+        'Estoy obesa',
+        'Soy asquerosa',
+        'Soy asqueroso',
+        'Me veo mal',
+        'Estoy gordo',
+        'Estoy gorda'
+    ])
+    .addAnswer(
+        [
+            '¡No creo que sea verdad!',
+            'Viniste a este mundo a ser feliz, no a buscar la perfección en tu cuerpo, eso solo puede conducirte a sufrir.'
+        ])
 
-
-const orientacion = addKeyword(['No es suficiente tu ayuda', 'Necesito mayor apoyo' , 'Necesito acudir con un profesional' , 'Ya no puedo mas con esto', 'Creo que necesito mas ayuda','Me podrias ayudar mas','Necesito mas apoyo','Conoces a alguien mas que me pueda ayudar'])
-    .addAnswer(['No te preocupes te proporcionaremos una lista con los contactos de profesionales que te podran brindar un mayor apoyo. Acude con ellos cuanto antes. RECUERDA NO ESTAS SOL@.'])
-    .addAnswer(['(LISTA DE CONTACTOS)'])
-
-//CREACIÓN DE INTENT "PETICION DE AYUDA"
-
-
-
-const peticionde_ayuda= addKeyword(['¿Que debo hacer?', 'Tengo dudas', 'Estoy intranquila','Quiero platicar'])
-    .addAnswer(['No te preocupes estamos para ayudarte, recuerda que somos tus amigas y estamos simpre para ti. Vamos a platicar va.'])
-
-
-//CORRECCIÓN DE INTENTS AVANCE ANTERIOR
-
-//CREACIÓN DE INTENT " SALUDO"
-
-
-
-const saludo = addKeyword(['Hola', 'Hi' , 'Holis' , 'Qué tal'])
-    .addAnswer(['Bienvenid@, somos Ana y Mia . ', '¿Como podemos ayudarte?'])
-
-
-//CREACIÓN DE INTENT "DESPEDIDA"
-
-
-
-const DESPEDIDA= addKeyword(['Adiós','Adiós', 'Bye' , 'Hasta luego' , 'Nos vemos'])
-    .addAnswer(['Nos vemos pronto.'])
-    .addAnswer(['Fue un gusto saludarte :)' ])
-
-//CREACIÓN DE INTENT "Sentimientos negativos"
-
-
-const sentimiento = addKeyword(['Me siento mal', 'Es un mal dia' , 'No me encuentro nada bien' , 'Estoy mal', 'Estoy triste'])
-    .addAnswer(['No te preocupes,todo mejorará'])
-    .addAnswer(['¿Te gustaría platicar con nosotras?'])
-
-
-//CREACIÓN DE INTENT "Sentimientos positivo"
-
-
-
-const sentimientop = addKeyword(['Me siento bien', 'Es un buen dia' , 'Me encuentro  bien' , 'Estoy bien', 'Estoy feliz'])
-    .addAnswer(['Nos alegramos mucho por ti. Sigue teniendo un bello dia. :) '])
-
+const flowPrincipal = addKeyword(
+    [
+        'Que tal',
+        'hola,hola',
+        'Hey',
+        'Hola',
+        'Que hay'
+    ])
+    .addAnswer(
+        [
+            '¡Hola!, que tal',
+            'Que gusto saludarte'
+        ])
+    .addAnswer('¿Cómo te encuentras el día de hoy?', {
+        delay: 1500
+    }, null, [flowBat, flowGood]);
 
 const main = async () => {
-    const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([saludo,sentimiento,DESPEDIDA,peticionde_ayuda,orientacion])
+    const adapterDB = new MongoAdapter({
+        dbUri: MONGO_DB_URI,
+        dbName: MONGO_DB_NAME,
+    })
+    const adapterFlow = createFlow([flowPrincipal, flowAutoAtact])
     const adapterProvider = createProvider(BaileysProvider)
-
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
     })
-
+    //createBotDialog({
+    //    provider: adapterProvider,
+    //    database: adapterDB,
+    //});
     QRPortalWeb()
 }
 
---
+++ app2.js
@@ -0,0 +1,76 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+
+const botondesentimiento= addKeyword('¿Cómo te sientes ?').addAnswer('Elige tu estado de animo', {
+    buttons: [{ body: 'Bien :)' }, { body: 'Mal :(' }, { body: 'Triste :((' }, { body: 'Muy Feliz :)' }, { body: 'No muy bien :Z' }],
+})
+
+//CREACIÓN DE INTENT " ORIENTACION CON PROFESIONALES"
+
+
+
+const orientacion = addKeyword(['No es suficiente tu ayuda', 'Necesito mayor apoyo' , 'Necesito acudir con un profesional' , 'Ya no puedo mas con esto', 'Creo que necesito mas ayuda','Me podrias ayudar mas','Necesito mas apoyo','Conoces a alguien mas que me pueda ayudar'])
+    .addAnswer(['No te preocupes te proporcionaremos una lista con los contactos de profesionales que te podran brindar un mayor apoyo. Acude con ellos cuanto antes. RECUERDA NO ESTAS SOL@.'])
+    .addAnswer(['(LISTA DE CONTACTOS)'])
+
+//CREACIÓN DE INTENT "PETICION DE AYUDA"
+
+
+
+const peticionde_ayuda= addKeyword(['¿Que debo hacer?', 'Tengo dudas', 'Estoy intranquila','Quiero platicar'])
+    .addAnswer(['No te preocupes estamos para ayudarte, recuerda que somos tus amigas y estamos simpre para ti. Vamos a platicar va.'])
+
+
+//CORRECCIÓN DE INTENTS AVANCE ANTERIOR
+
+//CREACIÓN DE INTENT " SALUDO"
+
+
+
+const saludo = addKeyword(['Hola', 'Hi' , 'Holis' , 'Qué tal'])
+    .addAnswer(['Bienvenid@, somos Ana y Mia . ', '¿Como podemos ayudarte?'])
+
+
+//CREACIÓN DE INTENT "DESPEDIDA"
+
+
+
+const DESPEDIDA= addKeyword(['Adiós','Adiós', 'Bye' , 'Hasta luego' , 'Nos vemos'])
+    .addAnswer(['Nos vemos pronto.'])
+    .addAnswer(['Fue un gusto saludarte :)' ])
+
+//CREACIÓN DE INTENT "Sentimientos negativos"
+
+
+const sentimiento = addKeyword(['Me siento mal', 'Es un mal dia' , 'No me encuentro nada bien' , 'Estoy mal', 'Estoy triste'])
+    .addAnswer(['No te preocupes,todo mejorará'])
+    .addAnswer(['¿Te gustaría platicar con nosotras?'])
+
+
+//CREACIÓN DE INTENT "Sentimientos positivo"
+
+
+
+const sentimientop = addKeyword(['Me siento bien', 'Es un buen dia' , 'Me encuentro  bien' , 'Estoy bien', 'Estoy feliz'])
+    .addAnswer(['Nos alegramos mucho por ti. Sigue teniendo un bello dia. :) '])
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([saludo,sentimiento,DESPEDIDA,peticionde_ayuda,orientacion])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
